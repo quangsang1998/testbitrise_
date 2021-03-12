e86e040d84ef7e204e0d3deeda0a022498b6c66f
Hoàng Bình Dương <duonghb.coltech@gmail.com>
2021-03-12 16:29:59 +0700

    message

--- app/src/main/java/com/duonghb/testbitrise/MainActivity.kt
+++ app/src/main/java/com/duonghb/testbitrise/MainActivity.kt
@@ -23,6 +23,6 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        //comment 2
+        //comment 2 3
     }
 }

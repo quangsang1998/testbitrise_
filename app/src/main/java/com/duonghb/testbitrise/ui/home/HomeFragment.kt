5d383f13de179854eaea324ed887809f894c13a5
Hoàng Bình Dương <duonghb.coltech@gmail.com>
2021-03-15 11:05:38 +0700

    gem file

--- app/src/main/java/com/duonghb/testbitrise/ui/home/HomeFragment.kt
+++ app/src/main/java/com/duonghb/testbitrise/ui/home/HomeFragment.kt
@@ -26,7 +26,7 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
-        //eroror 22222 4444
+        //eroror 22222 4444 66
         return root
     }
 }

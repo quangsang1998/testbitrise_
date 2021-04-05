a89e5354317e6461e513ab27e199751e9b9e4b43
Hoàng Bình Dương <duonghb.coltech@gmail.com>
2021-04-05 13:16:07 +0700

    test danger ok

--- app/src/main/java/com/duonghb/testbitrise/ui/home/HomeFragment.kt
+++ app/src/main/java/com/duonghb/testbitrise/ui/home/HomeFragment.kt
@@ -15,18 +15,21 @@ class HomeFragment : Fragment() {
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        //eroror 22222 4444 66 88 11 222 4 8 7 7 8 8 1 1 2 1 1 1 1
+        homeViewModel.text.observe(
+            viewLifecycleOwner,
+            Observer {
+                textView.text = it
+            }
+        )
+        //eroror 22222 4444 66 88 11 222 4 8 7 7 8 8 1 1 2 1 1 1 1 2
         return root
     }
 }

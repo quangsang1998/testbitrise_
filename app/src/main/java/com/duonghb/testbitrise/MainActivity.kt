58a440f875a0ab69fef317343ea38a35eca6ba7d
Hoàng Bình Dương <duonghb.coltech@gmail.com>
2021-05-05 11:42:46 +0700

    test danger ok

--- app/src/main/java/com/duonghb/testbitrise/MainActivity.kt
+++ app/src/main/java/com/duonghb/testbitrise/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //check looix 4433
+        //check looix 4433
     }
 }

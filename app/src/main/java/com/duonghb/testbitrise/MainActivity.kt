dc568d6a96da6bc4dbdb3d3664592d5103132aa1
Hoàng Bình Dương <duonghb.coltech@gmail.com>
2021-05-05 14:15:16 +0700

    test danger ok

--- app/src/main/java/com/duonghb/testbitrise/MainActivity.kt
+++ app/src/main/java/com/duonghb/testbitrise/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //test 1
+        //test 1 2 3
     }
 }

865a54d24182e1f9dcca0d0cf3d4de7a7b81e076
Do Quang Sang <sangdq@hybrid-technologies.vn>
2021-05-19 14:52:10 +0700

    delete .idea/compiler.xml

--- app/src/main/java/com/duonghb/testbitrise/domain/repository/NewsRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.duonghb.testbitrise.domain.repository
-
-import com.duonghb.testbitrise.domain.model.NewsModel
-import io.reactivex.Single
-
-interface NewsRepository {
-    fun getNewsList(): Single<List<NewsModel>>
-}

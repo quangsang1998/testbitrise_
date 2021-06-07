063e1db239dd9a1ffde092dd9b1d5e0effeb8d2e
Do Quang Sang <sangdq@hybrid-technologies.vn>
2021-06-07 16:42:16 +0700

    app-news

--- app/src/main/java/com/duonghb/testbitrise/ui/home/NewsAppGlideModule.kt
+++
@@ -1,7 +0,0 @@
-package com.duonghb.testbitrise.ui.home
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class NewsAppGlideModule : AppGlideModule()
